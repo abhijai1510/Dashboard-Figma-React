f43956b3e0f238293c7d39617e3c59d69a53b9ea
Abhijai Srivastava <abhijai.219301464@muj.manipal.edu>
2025-01-12 14:57:08 +0530

    Final commit

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -8,12 +8,12 @@ const SidebarNav = () => {
       <div className="sidebar-content">
         <div className="profile">
           <img 
-            src="https://via.placeholder.com/96" 
-            alt="Samantha" 
+            src="https://media.licdn.com/dms/image/v2/D5603AQGM5pMCtnT4Gw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725103002572?e=1742428800&v=beta&t=WI7Q27lqER8qraPpR5FpuKc3mPEFJALBgolF4QmWRVw" 
+            alt="Abhijai" 
             className="avatar"
           />
-          <h2 className="username">Samantha</h2>
-          <p className="email">samantha@email.com</p>
+          <h2 className="username">Abhijai</h2>
+          <p className="email">xyz@email.com</p>
         </div>
         
         <nav className="nav-menu">
